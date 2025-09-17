c607d8a43c344e533d2a0f0d9a4d8fe4059804eb
LisaxDsouza <lisaxhaze@gmail.com>
2025-09-17 14:22:09 +0530

    Revert "Initial commit - frontend"
    This reverts commit 9f8969e85f80be5ca2f807547d4975ac2fe989b4.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import FacultyDashboard from "./pages/FacultyDashboard";
-import CoordinatorDashboard from "./pages/CoordinatorDashboard";
-import AdminDashboard from "./pages/AdminDashboard";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/faculty" element={<FacultyDashboard />} />
-        <Route path="/coordinator" element={<CoordinatorDashboard />} />
-        <Route path="/admin" element={<AdminDashboard />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
